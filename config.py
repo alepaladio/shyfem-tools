4b39509bb591f8589c3ebaf81655b71f5af172a0
Ale Paladio <alejandropaladio@gmail.com>
2026-02-05 14:25:55 +0100

    Retaking the creation

---
+++ config.py
@@ -0,0 +1,14 @@
+"""
+Configuration for variable name mappings
+"""
+
+VARIABLE_MAPPINGS = {
+    'longitude': ['longitude', 'Mesh2_node_x', 'lon', 'x'],
+    'latitude': ['latitude', 'Mesh2_node_y', 'lat', 'y'],
+    'salinity': ['salinity', 'salt', 'so'],
+    'temperature': ['temperature', 'temp', 'thetao'],
+    'time': ['time', 't', 'time_counter'],
+    'level': ['level', 'depth', 'z', 'nav_lev']
+}
+
+# More will be added later
