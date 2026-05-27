224e92fb43981e1fded4511ba4ed4495a1eca4e5
Ale Paladio <alejandropaladio@gmail.com>
2026-05-27 15:39:41 +0200

    Modify2 file I/O utilities to read/write shyfem format files.

--- shyfem/io/__init__.py
+++ shyfem/io/__init__.py
@@ -4,12 +4,14 @@ I/O operations for SHYFEM files
 
 # Import everything from your modules
 from .nc_node_extractor import SHYFEMNodeExtractor, extract_river_transect
+from .shy_reader import SHYReader, read_shy_file
+from .shy_writer import SHYWriter, write_shy_file
 
 # List what's available in this submodule
 __all__ = [
     'SHYFEMNodeExtractor',
     'extract_river_transect',
-        'SHYReader',
+    'SHYReader',
     'read_shy_file',
     'SHYWriter',
     'write_shy_file',
